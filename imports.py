7b0fa0b58858ca73ea976200aa403631cb396d65
Cristiancrc97 <cristiancrc97@hotmail.com>
2016-11-05 12:59:01 -0500

    [Añadido] Archivo imports que posteriormente contendrá todos los objetos (Añadidas los imports de las librerias necesarias)

---
+++ imports.py
@@ -0,0 +1,8 @@
+try:
+    import pygame,sys,random,threading,time,ConfigParser
+    from pygame.locals import *
+except (KeyboardInterrupt, SystemExit): 
+        raise
+except:
+    print("Error: No se lograron importar las librerias correctamente")
+    raise
