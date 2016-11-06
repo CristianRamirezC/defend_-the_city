e8c092ea97d66aa44dac1187600de6bd7cf08f67
Cristiancrc97 <cristiancrc97@hotmail.com>
2016-11-06 15:02:04 -0500

    [ADD] Implementado el menu principal, movimiento entre opciones con resaltado

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
 from imports import *
-pygame.mixer.init()
 
-if not pygame.display.get_init():
-    pygame.display.init()
-if not pygame.font.get_init():
-    pygame.font.init()
-global ANCHO,ALTO,pantalla
-ANCHO=800
-ALTO=600
-pantalla = pygame.display.set_mode((ANCHO,ALTO))
-pantalla.fill((51,51,51))
-m =Menu(pantalla)
-m.draw_menu()
+def start():
+    pygame.mixer.init()
+    if not pygame.display.get_init():
+        pygame.display.init()
+    if not pygame.font.get_init():
+        pygame.font.init()
+    global ANCHO,ALTO,pantalla
+    ANCHO=800
+    ALTO=600
+    pantalla = pygame.display.set_mode((ANCHO,ALTO))
+    pantalla.fill((51,51,51))
+    m =Menu(['Iniciar juego','Controles', 'Salir'], pantalla, (ANCHO/2-150,ALTO/2-50))
+    m.tam_font = 68
+    m.draw_menu()
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                sys.exit()
+    pygame.display.update()
+    while 1:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_DOWN:
+                    if(m.cursor >= len(m.lista)-1):
+                        m.cursor=0
+                    else:
+                        m.cursor += 1
+                    pantalla.fill((51,51,51))
+                    m.draw_menu()
+
+                if event.key == K_UP:
+                    if(m.cursor <= 0):
+                        m.cursor=len(m.lista)-1
+                    else:
+                        m.cursor -= 1
+                    pantalla.fill((51,51,51))
+                    m.draw_menu()
+                if event.key == K_RETURN:
+                    if m.cursor == 2:
+                        sys.exit(0)
+                    if m.cursor == 0:
+                        print("Juego")
+                    if m.cursor == 1:
+                        print("Menu help")
+                pantalla.fill((51,51,51))
+                m.draw_menu()
+
+    return 1
+
+if __name__ == "__main__":
+    start()
