752212f953189061d11559c074f1b68cc03821d0
Cristiancrc97 <cristiancrc97@hotmail.com>
2016-11-07 15:28:35 -0500

    [ADD] Añadida y actualizada la clase elemento

--- imports.py
+++ imports.py
@@ -8,6 +8,21 @@ except:
     raise
 
 
+class Elemento(pygame.sprite.Sprite):
+    def __init__(self, x, y, archivo):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(archivo).convert()
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+        self.tipo = "ninguno"
+        self.actualizable = "no"
+
+    def update_rect(self,x,y):
+        self.rect = self.image.get_rect()
+        self.rect.x= x
+        self.rect.y= y
+
 class Menu:
     lista = []
     tam_font = 32
